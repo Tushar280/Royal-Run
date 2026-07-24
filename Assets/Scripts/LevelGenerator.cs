7183ea7bc15518d80e76abd1ad7475c161e9165d
Tushar280 <chaudarytushar280@gmail.com>
2026-07-24 22:49:34 +0530

    Make chunk instantiation dynamic
    Changed from a hardcoded prefab instance in the scene to dynamically instantiating the chunk prefab via a SerializeField in LevelGenerator. This provides more flexibility for level generation and makes the prefab easier to configure in the inspector.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject chunkPrefab;
+    
     void Start()
     {
-        for(int i = 0; i < 5; i++)
-        {
-            Debug.Log("Level Generated"+i);
-        }
+        Instantiate(chunkPrefab, transform.position, Quaternion.identity);
     }
 }
