9a4a71e5ab78b1c6a86ad8b7b60e0fbe712a55b0
Tushar280 <chaudarytushar280@gmail.com>
2026-07-24 22:51:30 +0530

    Update LevelGenerator.cs

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] GameObject chunkPrefab;
+    [SerializeField] int initialChunkAmt = 10;
     
     void Start()
     {
+        for(int i =0;i < initialChunkAmt;i++)
         Instantiate(chunkPrefab, transform.position, Quaternion.identity);
     }
 }
