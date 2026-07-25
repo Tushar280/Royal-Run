8e9508837af11579a48162736682b9616ac143bb
Tushar280 <chaudarytushar280@gmail.com>
2026-07-25 16:42:53 +0530

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,10 +6,25 @@ public class Movement : MonoBehaviour
 
     Rigidbody rb;
     Vector2 move;
+    [SerializeField] float speed = 10f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 currentPosition = rb.position;
+        Vector3 moveDirection = new Vector3(move.x,0,move.y);
+        Vector3 newPosition = currentPosition + (moveDirection * speed * Time.fixedDeltaTime);
+
+        rb.MovePosition(newPosition);
     }
 
     public void Move(InputAction.CallbackContext context)
