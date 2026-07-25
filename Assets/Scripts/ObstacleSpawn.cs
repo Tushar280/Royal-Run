56408be6e57e3545675e623da7d59c9b0d00374d
Tushar280 <chaudarytushar280@gmail.com>
2026-07-25 23:09:50 +0530

    Add staggered obstacle spawning with delay
    Convert obstacle spawning from instantaneous to coroutine-based, allowing obstacles to be spawned with a configurable delay between each spawn. This prevents all obstacles from being created simultaneously. Also adjust physics gravity z-component from -9.81 to -11 for physics tuning.

--- Assets/Scripts/ObstacleSpawn.cs
+++ Assets/Scripts/ObstacleSpawn.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
 
 public class ObstacleSpawn : MonoBehaviour
 {
     [SerializeField] GameObject obstacle;
     [SerializeField] int numToSpawn = 10;
+    [SerializeField] int waitPerSpawn = 3;
 
     private void Start()
     {
-        for (int i = 0; i < numToSpawn; i++)
+        StartCoroutine(SpawnObstacles());
+    }
+
+
+    IEnumerator SpawnObstacles()
+    {
+        
+        while(numToSpawn > 0)
         {
             Instantiate(obstacle,transform.position,Quaternion.identity);
+            numToSpawn--;
+            yield return new WaitForSeconds(waitPerSpawn);
         }
     }
 }
