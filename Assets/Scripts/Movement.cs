c82ade59be5f2b3e6df37ad8ef2fe3146146ec04
Tushar280 <chaudarytushar280@gmail.com>
2026-07-25 16:35:54 +0530

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,7 +3,14 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+
+    Rigidbody rb;
     Vector2 move;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
