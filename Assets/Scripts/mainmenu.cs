7567b2eb765020f503d479e58a5bfbe79910840a
Tushar280 <chaudarytushar280@gmail.com>
2026-08-03 15:20:47 +0530

    Added Backgound image , buttons , mainmenu script

--- Assets/Scripts/mainmenu.cs
+++ Assets/Scripts/mainmenu.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class mainmenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
